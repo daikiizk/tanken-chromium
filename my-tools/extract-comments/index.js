08a0e9c3f6c9d02099341c24d806df07f32872ff
daikiizk <iizuka@daiiz.dev>
2024-09-29 15:21:37 +0900

    impl findFilesWithExtension

--- my-tools/extract-comments/index.js
+++ my-tools/extract-comments/index.js
@@ -1,7 +1,31 @@
 const fs = require("fs");
+const path = require("path");
 
 const chromiumPath = "/home/daiiz/daiiz2/chromium";
-const targetRootDirs = ["chrome"];
+const targetRootDirs = ["chrome", "ui"];
+const exts = [
+  //".cc",
+  //".h",
+  ".js",
+  ".ts",
+];
+
+async function findFilesWithExtension(dir, ext, fileList = []) {
+  const files = await fs.promises.readdir(dir);
+
+  for (const file of files) {
+    const fullPath = path.join(dir, file);
+    const stat = await fs.promises.stat(fullPath);
+
+    if (stat.isDirectory()) {
+      await findFilesWithExtension(fullPath, ext, fileList);
+    } else if (path.extname(fullPath) === ext) {
+      fileList.push(fullPath);
+    }
+  }
+
+  return fileList;
+}
 
 async function main() {
   const dirs = await fs.promises.readdir(chromiumPath);
@@ -11,6 +35,10 @@ async function main() {
     }
     const path = `${chromiumPath}/${dir}`;
     console.log(`Processing: ${path}`);
+    for (const ext of exts) {
+      const filePaths = await findFilesWithExtension(path, ext);
+      console.log(dir, ext, filePaths.length);
+    }
   }
 }
 
